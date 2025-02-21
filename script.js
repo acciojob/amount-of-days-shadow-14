7ff82559979a49c685b81640869f49e68a38efe7
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-02-21 08:58:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function daysOfAYear(n) {
+	let ans = 0;
+	 if (n % 4 == 0) {
+     
+      // If it's divisible by 100, it should also be 
+      // divisible by 400 to be a leap year
+      if (n % 100 == 0) {
+         ans= n % 400 == 0;
+      }
+      ans= true;
+   }
+   ans = false;
+	if(ans)return 366;
+	return 365;
+}
